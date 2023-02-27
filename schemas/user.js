858cae0f6e5a46a82b4398d797d71a343c9fd921
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2023-02-27 19:19:06 +0700

    create user schema

---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  title: 'User', // Model
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
